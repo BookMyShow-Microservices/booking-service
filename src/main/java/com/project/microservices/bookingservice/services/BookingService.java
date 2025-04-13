cbce1f4231650f1c7e157599b1e1c869684afa99
DummuMounika <mounikadummu.edu2024@gmail.com>
2025-04-13 16:45:53 -0500

    Declare the method

--- src/main/java/com/project/microservices/bookingservice/services/BookingService.java
+++ src/main/java/com/project/microservices/bookingservice/services/BookingService.java
@@ -9,11 +9,14 @@ import com.project.microservices.bookingservice.model.ShowDetails;
 
 public interface BookingService {
 	
+
 	public BookingSummaryResponse generateBookingSummary(BookingSummaryRequest bookingSummaryRequest);
 	
 	public ShowDetails fetchShowDetailsFromSearchService(Integer showId);
 	
 	public  String updateSeatStatus(List<Integer> seatUniqueId,Integer status,Integer showId);
 
-	public Integer createBooking(BookingDetails bookingTableRequest);		
+	public Integer createBooking(BookingDetails bookingTableRequest);
+
+	public void runScheduleTask();
 }
